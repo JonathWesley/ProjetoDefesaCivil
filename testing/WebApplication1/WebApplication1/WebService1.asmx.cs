b700468e3c993423ef2ed27f96041f7ba06e8a97
Jonath <jonathherdt@gmail.com>
2018-12-03 15:46:54 -0200

    .

---
+++ testing/WebApplication1/WebApplication1/WebService1.asmx.cs
@@ -0,0 +1,49 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.Services;
+namespace AspnWebServiceDB
+{
+    /// <summary>
+    /// Summary description for ServiceDB
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    //[System.Web.Script.Services.ScriptService]
+    public class ServiceDB : System.Web.Services.WebService
+    {
+        [WebMethod]
+        public DataTable GetDados()
+        {
+            string constr = ConfigurationManager.ConnectionStrings["conexaoProdutos"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                using (SqlCommand cmd = new SqlCommand("SELECT * FROM Produtos"))
+                {
+                    using (SqlDataAdapter sda = new SqlDataAdapter())
+                    {
+                        cmd.Connection = con;
+                        sda.SelectCommand = cmd;
+                        using (DataTable dt = new DataTable())
+                        {
+                            dt.TableName = "Produtos";
+                            sda.Fill(dt);
+                            return dt;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+</configuration
+   .....
+   .....
+  </system.codedom>
+  <connectionStrings>
+    <add name = "conexaoProdutos" connectionString="Data Source=MACORATTI;Initial Catalog=Cadastro;Integrated Security=True" providerName="System.Data.SqlClient"/>
+  </connectionStrings>
+</configuration
