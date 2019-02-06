f06cf0942440eec521431e50f6d3c24e5a35b667
jonath <jonathherdt@gmail.com>
2019-02-06 18:50:42 -0200

    .

--- HTML pages/angular.js
+++ HTML pages/angular.js
@@ -3,7 +3,12 @@ var app = angular.module("myApp", []);
 app.controller("myCtrl", function($scope){
     $scope.telefone = "(47) 3268-3133";
     $scope.endereco = "R. Pardal, 111 - Ariribá, Balneário Camboriú - SC";
+    $scope.pesquisa = "";
+    $scope.show = false;
     $scope.caminho = [{name:"Home", route:"main.html"}];
+    $scope.ocorrencias = [{id:"11111", cobrade:"Deslizamento", prioridade:"Alta"}, 
+                          {id:"22222", cobrade:"Enchente", prioridade:"Média"},
+                          {id:"33333", cobrade:"Incendio", prioridade:"Alta"}];
 
     $scope.login = function(){
         window.location = "main.html";
@@ -17,5 +22,8 @@ app.controller("myCtrl", function($scope){
     };
     $scope.perfil = function(){
         window.location = "perfil.html";
+    }
+    $scope.exibirOcorrencia = function(){
+        window.location = "exibirOcorrencia.html";
     }
 });
