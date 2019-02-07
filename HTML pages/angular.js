06e6b28bdae3685459c76abf0a407781c9a85b96
jonath <jonathherdt@gmail.com>
2019-02-07 18:33:17 -0200

    .

--- HTML pages/angular.js
+++ HTML pages/angular.js
@@ -1,17 +1,29 @@
 var app = angular.module("myApp", []);
 
 app.controller("myCtrl", function($scope){
+    $scope.usuario = "";
+    $scope.senha = "";
+    $scope.nivel = "";
     $scope.telefone = "(47) 3268-3133";
     $scope.endereco = "R. Pardal, 111 - Ariribá, Balneário Camboriú - SC";
     $scope.pesquisa = "";
-    $scope.show = false;
     $scope.caminho = [{name:"Home", route:"main.html"}];
     $scope.ocorrencias = [{id:"11111", cobrade:"Deslizamento", prioridade:"Alta"}, 
                           {id:"22222", cobrade:"Enchente", prioridade:"Média"},
                           {id:"33333", cobrade:"Incendio", prioridade:"Alta"}];
 
     $scope.login = function(){
-        window.location = "main.html";
+        if($scope.usuario=="admin" && $scope.senha=="senha"){
+            $scope.nivel = "admin";
+            window.location = "main.html";
+            alert("admin");
+        }else if($scope.usuario=="usuario" && $scope.senha=="senha"){
+            $scope.nivel = "usuario";
+            window.location = "main.html";
+            alert("usuario");
+        }else{
+            alert("Usuário ou senha errados");
+        }
     };
     $scope.changePassword = function(){
         alert('trocar a senha!');
@@ -22,8 +34,8 @@ app.controller("myCtrl", function($scope){
     };
     $scope.perfil = function(){
         window.location = "perfil.html";
-    }
+    };
     $scope.exibirOcorrencia = function(){
         window.location = "exibirOcorrencia.html";
-    }
+    };
 });
